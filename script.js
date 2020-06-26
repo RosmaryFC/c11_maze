6e5905e68e9a51cec6b1259c86d57a157e21eb61
ROSMARY FERMIN <rosmaryfc@hotmail.com>
2020-06-26 08:39:49 -0400

    fixed dot movement

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/*
+Author: Kenya, William, Rosemary
+Current iteration: Rosemary
+
+Blog: https://rosmaryfc.wordpress.com/2020/06/25/final-project-precourse/
+
+
+*/
+
+
 //shows where walls are (TRouBLe format)
 //t = top, r = right, b = bottom, l = left
  const mazeArr = [
@@ -45,7 +55,6 @@ function checkMove(position){
 //moves user position in array (TRouBLe format)
 // Every time user moves, all buttons dissapear and buttons reappear based on move
 function moveUser(){
-  /*down = row + 1, up = row -1, left = column -1, right = row + 1 */
   //make buttons invisible for unmovable positions
   let upButton = document.querySelector("#up");
   upButton.style.display='none';
@@ -66,8 +75,7 @@ function moveUser(){
   if(walls[1]){ // can move right
     //todo: enable right button / move right
     rightButton.style.display = 'inline-block';
-        console.log("show right btn");
-
+    console.log("show right btn");
   }
   if(walls[2]){ // can move down
     //todo: enable bottom button / move bottom
@@ -82,17 +90,22 @@ function moveUser(){
 
 }
 
-//pass in a string repesenting top, bottom, right, and left style for dot div
-function move(direction) {
+//pass in a string  to increase/decrease bottom and left stlye for dot div
+function moveDot(direction, distance, shiftUser) {
   let dot = document.querySelector('#dot');  
   console.log("direction: " + direction);
-  const addPixels = 160;
   const directionStr = dot.style[direction];
   let directionValue = Number((directionStr.slice(0, directionStr.length - 2 )));
-  directionValue += addPixels;
+  directionValue += distance;
   dot.style[direction] = directionValue + "px";
   console.log("direction updated: " + dot.style[direction]);
-  row--;
+  //shifts user in the array depending on button pressed
+  /*down = row + 1, up = row -1, left = column -1, right = row + 1 */
+  if(direction == 'bottom' || direction == 'top'){
+    row += shiftUser;
+  }else{ // if directions is left or right
+    column += shiftUser;
+  }
   index = mazeArr[row][column];
   walls = checkMove(index);
   moveUser();
@@ -103,7 +116,7 @@ document.querySelector("#up").onclick = function(e){
   debugger;
   console.log('pressed up btn');
   //moving up so pixels must be added to bottom style
-  move('bottom');
+  moveDot('bottom', 93, -1); 
   console.log("row: " + row + " column: " + column + " index: " + index +" walls: " + walls );
 }
 
@@ -111,7 +124,7 @@ document.querySelector("#right").onclick = function(e){
   debugger;
   console.log('pressed right btn');
   //moving right so pixels must be added to left style
-  move('left');
+  moveDot('left', 96, +1);
   console.log("row: " + row + " column: " + column + " index: " + index +" walls: " + walls );
 }
 
@@ -119,7 +132,7 @@ document.querySelector("#down").onclick = function(e){
   debugger;
   console.log('pressed down btn');
   //moving down so pixels must be added to up style
-  move('top');
+  moveDot('bottom', -93, +1);
   console.log("row: " + row + " column: " + column + " index: " + index +" walls: " + walls );  
 }
 
@@ -127,10 +140,28 @@ document.querySelector("#left").onclick = function(e){
   debugger;
   console.log('pressed left btn');
   //moving left so pixels must be added to right style
-  move('right');
+  moveDot('left', -96, -1);
   console.log("row: " + row + " column: " + column + " index: " + index +" walls: " + walls );
 }
 
 //todo: add reset() function so after user finished game they can start over
-//Todo: add congrats() function for when finish line is reached
 //todo: add gameOver() function for when game is over ex: you run out of moves and lose or you finish game.
+
+
+//Todo: add congrats() function for when finish line is reached
+function checkPosition(){
+  if(row == finalPositionRow && column == finalPositionColumn){
+    //congratulations!
+  }
+}
+
+// todo: on start screen, grab the height and width of maze,
+// divide the height by num rows to evenly divide vertically, 
+//divide the width by num columns to evenly divide horizontally
+function initGame(){
+  const mazeHeight = document.querySelector('#maze').clientHeight;
+  console.log(mazeHeight);
+
+  const mazeWidth = document.querySelector('#maze').clientWidth;
+  console.log(mazeWidth);
+}
